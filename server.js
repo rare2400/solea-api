95bd4a7dc4f36b99b74d1b0c1071182f39f82a3c
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-27 17:36:36 +0200

    add connectiction to mongoDb plugin

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-'use strict';
+"use strict";
 
-import Fastify from "fastify";
-import dotenv from "dotenv";
+import Fastify from 'fastify';
+import dotenv from 'dotenv';
+import mongoDb from './src/plugins/mongodb.js';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -9,20 +10,22 @@ dotenv.config();
 // Fastify server instance with logging enabled
 const fastify = Fastify({ logger: true });
 
+await fastify.register(mongoDb);
+
 // Start the server with error handling
 const start = async () => {
-  const port =  process.env.PORT || 3000;
+    const port = process.env.PORT || 3000;
 
-  try {
-    await fastify.listen({ 
-      port: port,
-      host: "0.0.0.0"
-    });
-    console.log(`Server is running on port ${ port }`);
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
+    try {
+        await fastify.listen({
+            port: port,
+            host: "0.0.0.0"
+        });
+        console.log(`Server is running on port ${port}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
 }
 
 start();
