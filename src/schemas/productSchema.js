a6893e9d83a4c66d7800417d6d3078a7b7dd42e5
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-29 11:40:11 +0200

    edit categoryId to category and make it required.

--- src/schemas/productSchema.js
+++ src/schemas/productSchema.js
@@ -1,13 +1,14 @@
 // JSON schema for validating product data
 export const productSchema = {
     type: "object",
-    required: ["name, price, stock"],
+    required: ["name", "price", "stock", "category"],
     properties: {
         name: { type: "string", minLength: 1, maxLength: 100 },
         description: { type: "string", maxLength: 500 },
         price: { type: "number", minimum: 0 },
         image_url: { type: "string" },
         stock: { type: "integer", minimum: 0 },
-        categoryId: { type: "string", minLength: 1 }
-    }
-};
+        category: { type: "string", minLength: 1 }
+    },
+    additionalProperties: false
+}
